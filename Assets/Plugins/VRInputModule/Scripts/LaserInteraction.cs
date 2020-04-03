d6fff1fa47df99d0579554a783a07eece44d168c
JackGreene39 <jackmgreene39@gmail.com>
2020-04-03 18:26:08 -0500

    Added new scene with arm animation on button click

---
+++ Assets/Plugins/VRInputModule/Scripts/LaserInteraction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+using Wacki;
+
+public class LaserInteraction : MonoBehaviour
+{
+    public ViveUILaserPointer laserPointerLeft;
+    public ViveUILaserPointer laserPointerRight;
+    //public AnimControl controller;
+    void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        /*if (laserPointerLeft.ButtonDown() && laserPointerLeft.interactable != null)
+        {
+            Debug.Log(laserPointerLeft.interactable.tag);
+        } else if (laserPointerRight.ButtonDown() && laserPointerRight.interactable != null)
+        {
+            Debug.Log(laserPointerLeft.interactable.tag);
+        }*/
+    }
+
+    public void PointerClickAndHold(object sender, Valve.VR.Extras.PointerEventArgs e)
+    {
+        //if (e.target.GetComponent<continuousRotateObject>())
+        //{
+            Debug.Log("Rotating object");
+            
+        //} 
+    }
+
+    public void PointerClick(object sender, Valve.VR.Extras.PointerEventArgs e)
+    {
+        Button button = e.target.GetComponent<Button>();
+        if (button)
+        {
+            button.onClick.Invoke();
+        }
+        else
+        {
+            Debug.Log(e.target.name);
+            //controller.nullSpeed();
+        }
+    }
+
+    public void PointerInside(object sender, Valve.VR.Extras.PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, Valve.VR.Extras.PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+    }
+}
---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationController : MonoBehaviour
+{
+    public Button ArmButton;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = 1.0f;
+    }
+
+    public void PlayArmAnimation()
+    {
+       animator.SetBool("ArmAnimation", true);
+        ArmButton.gameObject.SetActive(false);
+    }
+
+    public void EndArmAnimation()
+    {
+        animator.SetBool("ArmAnimation", false);
+        ArmButton.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
